e79038500dd30c89763a83c2854a22e461a18a1d
gonzacucaita <gcucaita@unbosque.edu.co>
2018-05-08 01:49:00 -0500

    Sacrificio

--- src/com/unbosque/edu/co/calendarioFifa/beans/UserBean.java
+++ src/com/unbosque/edu/co/calendarioFifa/beans/UserBean.java
@@ -12,18 +12,16 @@ import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
-import javax.mail.Message;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.primefaces.event.FlowEvent;
 
 import com.unbosque.edu.co.calendarioFifa.entity.Audit;
+import com.unbosque.edu.co.calendarioFifa.entity.Parameter;
 import com.unbosque.edu.co.calendarioFifa.entity.User;
 import com.unbosque.edu.co.calendarioFifa.service.AuditService;
+import com.unbosque.edu.co.calendarioFifa.service.ParameterService;
 import com.unbosque.edu.co.calendarioFifa.service.UserService;
 import com.unbosque.edu.co.calendarioFifa.util.Correo;
 import com.unbosque.edu.co.calendarioFifa.util.Util;
@@ -31,83 +29,105 @@ import com.unbosque.edu.co.calendarioFifa.util.Util;
 @ManagedBean
 @SessionScoped
 public class UserBean {
-//Hola se esta actualizando******************************
-	//hola ahora soy migeul"""""""""""""""""""""
+	private static int INTENTOS = 0;
 	private User usuario;
+	private Parameter parameter = new Parameter();
+	private ParameterService parameterService = new ParameterService();
 	private DataModel listaUsuarios;
 	private String ingresarUsuario;
 	private String contrasenia;
-	
+
 	private static Logger log = Logger.getLogger(UserBean.class);
+
 	public String getIngresarUsuario() {
 		return ingresarUsuario;
 	}
+
 	public void setIngresarUsuario(String usuario) {
 		this.ingresarUsuario = usuario;
 	}
-	
+
 	public String getContrasenia() {
 		return contrasenia;
 	}
-	
+
 	public void setContrasenia(String contrasenia) {
 		this.contrasenia = contrasenia;
 	}
-	
+
 	public String validarUsuario() {
-	
-//		BasicConfigurator.configure();
+
+		BasicConfigurator.configure();
 		String respuesta = "registro";
 		Iterator<User> aux = getListarUsuario().iterator();
 		boolean existe = false;
-		
-//		 FacesMessage msg = new FacesMessage("Exito", "Bienvenido :" + usuario.getUserName());
-//	     FacesContext.getCurrentInstance().addMessage(null, msg);
-		
+
+		// FacesMessage msg = new FacesMessage("Exito", "Bienvenido :" +
+		// usuario.getUserName());
+		// FacesContext.getCurrentInstance().addMessage(null, msg);
+
 		contrasenia = Util.getStringMessageDigest(contrasenia, Util.MD5);
-		
-		while(aux.hasNext() && existe == false) {
+
+		while (aux.hasNext() && existe == false) {
+
+			usuario = aux.next();
+
+			boolean contra = usuario.getPassword().equals(contrasenia);
+
+			boolean use = usuario.getUserName().equals(ingresarUsuario);
 			
-		  usuario = aux.next();
-		
-		boolean contra = usuario.getPassword().equals(contrasenia);
-		
-		boolean use = usuario.getUserName().equals(ingresarUsuario);
-		
-		
-		if(contra && use) {
-			if(usuario.getUserType().equals("ADMIN")) {
-				respuesta = "/Administrador/administrador";
-			}
-			else if(usuario.getUserType().equals("FUNCIONAL")) {
-				respuesta = "/UserFuncional/funcional";
-			}
-			else {
-				respuesta = "/User/cliente";
-			}
+			int contador = 0;
 			
-			Audit auditoria = new Audit();
-			AuditService as = new AuditService();
-			auditoria.setUserId(usuario.getId());
-			auditoria.setOperation("E");
-			auditoria.setTableName("user");
-			auditoria.setTableId(1);
-			auditoria.setCreateDate(new Date());
-			as.save(auditoria);
-			if(log.isInfoEnabled()) {
-				log.info("Ingreso de usuario correcto : Usuario: "+usuario.getUserName()+" tipo: "+ usuario.getUserType());
-			}
-			existe = true;
+			if (contra && use && usuario.getActive().equals("A")) {
+				if (usuario.getUserType().equals("ADMIN")) {
+					respuesta = "/Administrador/administrador";
+				} else if (usuario.getUserType().equals("FUNCIONAL")) {
+					respuesta = "/UserFuncional/funcional";
+				} else {
+					respuesta = "/User/cliente";
+				}
+
+				Audit auditoria = new Audit();
+				AuditService as = new AuditService();
 			
-		
-		}
+				
+				auditoria.setUserId(usuario.getId());
+				auditoria.setOperation("E");
+				auditoria.setTableName("user");
+				auditoria.setTableId(1);
+				auditoria.setCreateDate(new Date());
+				as.save(auditoria);
+				if (log.isInfoEnabled()) {
+					log.info("Ingreso de usuario correcto : Usuario: " + usuario.getUserName() + " tipo: "
+							+ usuario.getUserType());
+				}
+				existe = true;
+
+			}
+			else if(use && contra==false && usuario.getActive().equals("A") ) {
+				INTENTOS++;
+				System.out.println(INTENTOS);
+				List<Parameter> lista = parameterService.list();
+//				for (int i = 0; i < lista.size() && existe==false; i++) {
+					if (INTENTOS==3) {
+						System.out.println("entra");
+						usuario.setActive("I");
+						respuesta = "/Error/ErrorLogin";
+						existe = true;
+					}	
+					else {
+						System.out.println("Hola");
+						respuesta = "/Principal/login";
+					}
+//				}
+				
+			}
 		}
 		contrasenia = "";
 		ingresarUsuario = "";
-		log.info("Ingreso del usuario Correcto: Usuario: "+ usuario.getUserName()+" Tipo: "+ usuario.getUserType() );
 		return respuesta;
 	}
-	
+
 	public String prepararAdicionarUsuario() {
 		usuario = new User();
 		usuario.setActive("A");
@@ -115,17 +135,17 @@ public class UserBean {
 		usuario.setUserType("cliente");
 		return "registro";
 	}
-	
+
 	public String prepararModificarUsuario() {
 		usuario = (User) (listaUsuarios.getRowData());
 		return "registro";
 	}
-	
+
 	public String eliminarUsuario() {
-		User usuarioTemp = (User)(listaUsuarios.getRowData());
+		User usuarioTemp = (User) (listaUsuarios.getRowData());
 		UserService dao = new UserService();
 		usuarioTemp.setActive("I");
-//		dao.remove(usuario);
+		// dao.remove(usuario);
 		dao.update(usuarioTemp);
 		Audit auditoria = new Audit();
 		AuditService as = new AuditService();
@@ -138,52 +158,49 @@ public class UserBean {
 		auditoria.setId(usuario.getId());
 		return "inicio";
 	}
-	
+
 	public String adicionarUsuario() {
 		UserService dao = new UserService();
 		String contra = generarContrasenia();
 		usuario.setPassword(Util.getStringMessageDigest(contra, Util.MD5));
 		dao.save(usuario);
-		String de = "calendario.fifa.uelbosque@gmail.com";  
+		String de = "calendario.fifa.uelbosque@gmail.com";
 		String clave = "patatafrita";
 		String asunto = "CONFIRMACION REGISTRO CALENDARIO FIFA";
-		String mensaje = "CALENDARIO FIFA 2018 \n"
-				+ "\n"
-				+ "\n"
-				+ "Usuario: " + usuario.getFullName() + "\n"
-				+ "\n"
-				+ "\n"
-				+ "\n"
-				+ "su cuenta se ha generado exitosamente \n"
-				+ "\n"
-				+ "\n    "
-				+ "usuario: "+usuario.getUserName() +"\n    clave: "+ contra+"\n "
-						+ "\n"
-						+ "\n"
-						+ "\n"
-						+ "Le solicitamos que una vez ingrese, cambie su contraseña.\n"
-						+ "\n"
-						+ "\n"
-						+ "\n"
-						+ "\n"
-						+ "Att: administrador CalendarioFIFA";
-		Correo.enviarCorreo(de, usuario.getEmailAddress(), clave, asunto, mensaje);
+		String mensaje = "CALENDARIO FIFA 2018 \n" + "\n" + "\n" + "Usuario: " + usuario.getFullName() + "\n" + "\n"
+				+ "\n" + "\n" + "su cuenta se ha generado exitosamente \n" + "\n" + "\n    " + "usuario: "
+				+ usuario.getUserName() + "\n    clave: " + contra + "\n " + "\n" + "\n" + "\n"
+				+ "Le solicitamos que una vez ingrese, cambie su contraseña.\n" + "\n" + "\n" + "\n" + "\n"
+				+ "Att: administrador CalendarioFIFA";
+		
+		// Correo.enviarCorreo(de, usuario.getEmailAddress(), clave, asunto, mensaje);
 		Audit auditoria = new Audit();
+//		parameter = new Parameter();
+//		parameterService = new ParameterService();
 		AuditService as = new AuditService();
+		
+		
 		auditoria.setUserId(usuario.getId());
 		auditoria.setOperation("C");
 		auditoria.setTableName("user");
 		auditoria.setTableId(1);
 		auditoria.setCreateDate(new Date());
 		as.save(auditoria);
+		
+		parameter.setParameterType(usuario.getId()+"");
+		parameter.setTextValue("5");
+		parameter.setNumberValue(3);
+		
+		parameterService.save(parameter);
+		
 		return "inicio";
 	}
-	
+
 	public String modificarUsuario() {
 		UserService dao = new UserService();
 		Audit auditoria = new Audit();
 		AuditService as = new AuditService();
-		
+
 		auditoria.setUserId(usuario.getId());
 		auditoria.setOperation("U");
 		auditoria.setTableName("user");
@@ -193,37 +210,33 @@ public class UserBean {
 		dao.update(usuario);
 		return "inicio";
 	}
-	
+
 	public User getUsuario() {
 		return usuario;
 	}
-	
+
 	public void setUsuario(User usuario) {
 		this.usuario = usuario;
 	}
-	
+
 	public DataModel getListarUsuario() {
 		List<User> lista = new UserService().list();
 		listaUsuarios = new ListDataModel(lista);
 		return listaUsuarios;
 	}
-	
-public String generarContrasenia() {
-		
+
+	public String generarContrasenia() {
+
 		String contrasenia = "";
 		String caracteres = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz1234567890";
-		
+
 		for (int i = 0; i < 8; i++) {
-			char a = caracteres.charAt((int) (Math.random()*caracteres.length()));
+			char a = caracteres.charAt((int) (Math.random() * caracteres.length()));
 			contrasenia += a;
-			
+
 		}
-		
+
 		return contrasenia;
 	}
 
-
-
-
-	
 }
--- src/com/unbosque/edu/co/calendarioFifa/util/Correo.java
+++ src/com/unbosque/edu/co/calendarioFifa/util/Correo.java
@@ -5,9 +5,11 @@ import java.util.Properties;
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 
 public class Correo {
-//Si carga
 	public Correo() {
 		
 	}
